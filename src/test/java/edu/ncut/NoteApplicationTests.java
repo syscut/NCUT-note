d9176b88647379a194f0daed421c38fb4dbeee48
Syscut <kim23362611@gmail.com>
2024-11-29 15:06:37 +0800

    init

---
+++ src/test/java/edu/ncut/NoteApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ncut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
